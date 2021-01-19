5dab0dd47d6497d5e85975e0beff9e6ed46117d6
Oskar Olsson <oppo_oskar@hotmail.com>
2021-01-19 14:28:10 +0100

    Basic site, only staticRendering

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,110 @@
 import Head from 'next/head'
+import style from '../styles/index.module.sass'
+import {useState} from 'react'
+import classNames from "classnames/bind"
 
-export default function Home() {
+export async function getServerSideProps(context) {
+
+
+  const res = await fetch(`http://192.168.0.122:8081/api/job`, 
+  {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7',
+      'Content-Type': 'application/json',
+      "X-Api-Key": "BD86451B85D3458FA21FA8291188D4B0"
+    }
+  })
+  const data = await res.json()
+
+  console.log(data)
+
+  if (!data) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {data}, // will be passed to the page component as props
+  }
+}
+
+
+
+export default function Home({ data }) {
+  function getColorFromState(param) {
+    switch(param) {
+      case 'Printing':
+        return 'green';
+      case 'Pausing', 'Operational':
+        return 'orange';
+      case 'Cancelling', 'Error', 'Offline':
+        return 'red';
+      default:
+        return 'red';
+    }
+  }
+
+  function formatTime(seconds,state) {
+    if(seconds == null)
+    {
+      return "0 min";
+    }
+    let minutes = Math.floor(seconds/60);
+    if(minutes >= 1)
+    {
+      let hours = Math.floor(minutes/60);
+      if(hours >= 1)
+      {
+        let days = Math.floor(hours/24);
+        if(days >= 1)
+        {
+          return (days + "d, " + hours%24 + "h, " + minutes%60 + "m")
+        }
+        return (hours + "h, " + minutes%60 + "m")
+      }
+      return (minutes + "m");
+    }
+    return (seconds + "s")
+  }
+
+  let isRunning = data.state == "Printing" || "Pausing";
+
+
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Oskar Olsson's 3D printer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <div className={style.content} style={{"border-color": getColorFromState(data.state)}}>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+          <div className={style.state} style={{"background-color": getColorFromState(data.state)}}>
+            <h2>{data.state}</h2>
+          </div>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <div className={style.textWrapper}>
+            <h1>Currently printing: {isRunning ? "NaN" : data.job.file.name}</h1>
+            
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+            <h1>Print time left: {formatTime(data.progress.printTimeLeft)}</h1>
+            <h1>Total print time: {isRunning ? "NaN" : formatTime(data.job.estimatedPrintTime)}</h1>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+            <h1>Completion: {data.progress.completion}%</h1>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          </div>
+
+          <div className={style.stream}>
+            <iframe src="https://3dprinterstream.oskarolsson.com/?action=stream" scrolling="no" frameBorder="0"></iframe>
+          </div>
         </div>
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
+      
       </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   )
 }
