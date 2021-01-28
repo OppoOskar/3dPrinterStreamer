90a7922d9f6ae1eb0c2ca9b06bcce0b80ee8b67a
Oskar Olsson <oppo_oskar@hotmail.com>
2021-01-28 13:31:41 +0100

    Hotfix

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const next = require('next')
 const bodyParser = require('body-parser')
 
 const dev = process.env.APP_ENV !== 'production'
-console.log(process.env.OCTOPRINT_SETTINGS)
-const conf = {"env": {"OP_SETTINGS": process.env.OCTOPRINT_SETTINGS}};
+console.log(process.env.OCTOPRINT_APIKEY)
+const conf = {"env": {"OP_SETTINGS": {"APIKEY": process.env.OCTOPRINT_APIKEY, "IP": process.env.OCTOPRINT_IP, "PORT": process.env.OCTOPRINT_PORT}}};
 const app = next({dev, conf})
 const handle = app.getRequestHandler()
 
