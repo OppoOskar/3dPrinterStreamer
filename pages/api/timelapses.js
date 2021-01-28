66fddfc9f1ae889cda38e2489e2395442e3b8367
Oskar Olsson <oppo_oskar@hotmail.com>
2021-01-28 13:56:59 +0100

    Hotfix

--- pages/api/timelapses.js
+++ pages/api/timelapses.js
@@ -3,12 +3,13 @@ import path from 'path';
 
 export default async (req, res) => {
     const address = "http://" + process.env.OP_SETTINGS.IP + ":" + process.env.OP_SETTINGS.PORT + "/api/timelapse";
+    const API_KEY = process.env.OP_SETTINGS.APIKEY
     const ans = await fetch(address, 
     {
         headers: {
         'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7',
         'Content-Type': 'application/json',
-        "X-Api-Key": process.env.OP_SETTINGS.APIKEY
+        "X-Api-Key": API_KEY
         }
         
     })
